eecb9197a5e598303b4ba4cf3dd5148d9fa45b2d
MarceloHK1 <126252674+MarceloHK1@users.noreply.github.com>
2023-09-20 20:18:57 -0300

    Update 0914-ArvoreOrdem5.cpp

--- 0914-ArvoreOrdem5.cpp
+++ 0914-ArvoreOrdem5.cpp
@@ -23,8 +23,8 @@ struct no {
 	struct no *filho5;
 };
 
-struct no *inserir (struct no *raiz, int number) {															//Função para inserir um nó na árvore
-	if (raiz == NULL) {																						//Se a árvore estiver vazia
+struct no *inserir (struct no *raiz, int number) {							//Função para inserir um nó na árvore
+	if (raiz == NULL) {										//Se a árvore estiver vazia
 		raiz = (struct no *) malloc(sizeof(struct no));
 		
 		raiz -> numero = number;
@@ -33,11 +33,11 @@ struct no *inserir (struct no *raiz, int number) {															//Função para
 		raiz -> filho3 = NULL;
 		raiz -> filho4 = NULL;
 		raiz -> filho5 = NULL;
-	} else {																								//Se a árvore não estiver vazia
-		int sorteio = (rand() % 5);																			//Resto de um número aleatório / qntd de filhos
+	} else {											//Se a árvore não estiver vazia
+		int sorteio = (rand() % 5);								//Resto de um número aleatório / qntd de filhos
 		
-		if (sorteio == 0) {																					//função recursiva até achar um nó vazio
-			raiz -> filho1 = inserir(raiz -> filho1, number);												//insere no filho1 o número
+		if (sorteio == 0) {									//função recursiva até achar um nó vazio
+			raiz -> filho1 = inserir(raiz -> filho1, number);				//insere no filho1 o número
 		} else if (sorteio == 1) {
 			raiz -> filho2 = inserir(raiz -> filho2, number);											
 		} else if (sorteio == 2) {
@@ -51,13 +51,13 @@ struct no *inserir (struct no *raiz, int number) {															//Função para
 	return raiz;
 }
 
-void imprimir(struct no *raiz, char *endentacao) {															//Função que imprime a árvore
+void imprimir(struct no *raiz, char *endentacao) {							//Função que imprime a árvore
 	if (raiz != NULL) {
 		printf("%s%d\n", endentacao, raiz -> numero);	
 		
 		char *temp = (char *) malloc(1024 * sizeof(char));
-		strcpy(temp, endentacao);																			//copia o valor da endentacao para temp
-		strcat(temp, "---");																				//soma no temp, "---"
+		strcpy(temp, endentacao);								//copia o valor da endentacao para temp
+		strcat(temp, "---");									//soma no temp, "---"
 		
 		imprimir(raiz -> filho1, temp);
 		imprimir(raiz -> filho2, temp);
@@ -68,11 +68,11 @@ void imprimir(struct no *raiz, char *endentacao) {															//Função que 
 }
 
 int main () {
-	struct no *raiz = NULL;																					//Declaração de variáveis
+	struct no *raiz = NULL;										//Declaração de variáveis
 	time_t t;
 	char *endentacao = (char *) malloc(1024 * sizeof(char));
 	
-	srand((unsigned) time(&t));																				//Inicialização da aleatoriedade
+	srand((unsigned) time(&t));									//Inicialização da aleatoriedade
 	
 	strcpy(endentacao, "");
 
